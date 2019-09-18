dbd5eb5784088aca5bcf7d3711b471cab3b12c60
Mikhail <uiui34@mail.ru>
2019-09-18 18:05:22 +0700

    Исправил все ошибки

--- src/components/HoverBlock/HoverBlock.jsx
+++ src/components/HoverBlock/HoverBlock.jsx
@@ -2,44 +2,59 @@ import React from 'react';
 import './style.scss';
 
 class HoverBlock extends  React.Component {
+    state ={
+        enterDirection: '',
+        leaveDirection: '',
+    };
     blockRef = React.createRef();
-    state ={hoverSide: 0};
-    handleMouseEnter = (e) => {
+    hoverRef = React.createRef();
+
+    calculateDirection = (e) => {
         const mouseCor = {x: e.pageX, y: e.pageY};
         const blockCor = {
-            topLeft: {
-                x: this.blockRef.offsetLeft,
-                y: this.blockRef.offsetTop
+            startPos: {
+                x: this.blockRef.current.offsetLeft,
+                y: this.blockRef.current.offsetTop
             },
-            bottomLeft: {
-                x: this.blockRef.offsetLeft,
-                y: this.blockRef.offsetTop + 200
-            },
-            topRight: {
-                x: this.blockRef.offsetLeft + 200,
-                y: this.blockRef.offsetTop
-            },
-            bottomRight: {
-                x: this.blockRef.offsetLeft + 200,
-                y: this.blockRef.offsetTop + 200
-            },
-
+            endPos: {
+                x: this.blockRef.current.offsetLeft + 200,
+                y: this.blockRef.current.offsetTop + 200
+            }
         };
-        if(mouseCor.x <= blockCor.topRight.x && mouseCor.x >= blockCor.topLeft.x ) {
-            console.log('top');
+        if((mouseCor.x <= blockCor.endPos.x && mouseCor.x >= blockCor.startPos.x) && mouseCor.y <= blockCor.startPos.y + 20 ) {
+            return 'top';
         }
-        if(mouseCor.y >= blockCor.topLeft.y && mouseCor.y <= blockCor.bottomLeft.y ) {
-            console.log('left');
+        else if((mouseCor.x <= blockCor.endPos.x && mouseCor.x >= blockCor.startPos.x) && mouseCor.y >= blockCor.endPos.y - 20) {
+            return 'bottom';
+        }
+        else if (( mouseCor.y >= blockCor.startPos.y && mouseCor.y <= blockCor.endPos.y ) && mouseCor.x <= blockCor.startPos.x + 20) {
+            return 'left';
+        }
+        else if (( mouseCor.y >= blockCor.startPos.y && mouseCor.y <= blockCor.endPos.y ) && mouseCor.x <= blockCor.endPos.x) {
+            return 'right';
         }
     }
 
-    handleMouseLeave = () => {
-       this.setState({hoverSide: 0});
-    }
+    handleMouseEnter = (e) => {
+        this.hoverRef.current.classList.add(this.calculateDirection(e));
+        this.setState({enterDirection: this.calculateDirection(e)});
+    };
+
+    handleMouseLeave = (e) => {
+        this.hoverRef.current.classList.remove(this.state.enterDirection);
+    };
 
     render() {
+        const {enterDirection, leaveDirection} = this.state;
         return(
-            <div className="hoverableBlock" ref={this.blockRef} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}/>
+            <div
+                ref={this.blockRef}
+                className="hoverableBlock"
+                onMouseEnter={this.handleMouseEnter}
+                onMouseLeave={this.handleMouseLeave}
+            >
+                <div className="hoverBlock" ref={this.hoverRef}/>
+            </div>
         )
     }
 }
